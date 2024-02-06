dbbda21724be58823b0518a12085cffdf911b249
Henn Sarv <sarviktaat@hotmail.com>
2024-02-06 16:20:52 +0200

    lisasin teise harjutuse uuesti enne oli näpuk

--- Teine/Properties/AssemblyInfo.cs
+++ Teine/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("f7ddddf8-c2a2-4e65-a86a-8fb9df569166")]
+[assembly: Guid("e6c462a7-ffbc-41b3-91da-a296550d9374")]
 
 // Version information for an assembly consists of the following four values:
 //
