9cd2d6431c80184d0ea71e35a3f32934e97ab526
Henn Sarv <sarviktaat@hotmail.com>
2024-02-07 13:50:03 +0200

    veel natuke enummidest

--- LisaksKlasssidele/Program.cs
+++ LisaksKlasssidele/Program.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
 
 namespace LisaksKlassidele
 {
-    public enum Mast { Risti , Ruutu, Ärtu, Poti, Pada = 3}
+    public enum Mast { Risti, Ruutu, Ärtu, Poti, Pada = 3 }
     public enum Kaart { Äss, Kaks, Kolm, Neli, Viis, Kuus, Seitse, Kaheksa, Üheksa, Kümme, Soldat, Emand, Kuningas }
 
     [Flags]
@@ -17,34 +19,54 @@ namespace LisaksKlassidele
         static void Main(string[] args)
         {
             Mast mast = Mast.Ärtu;
-            Console.WriteLine( mast );
+            Console.WriteLine(mast);
             mast++;
-            Console.WriteLine( mast);
+            Console.WriteLine(mast);
 
             Random R = new Random();
 
-            for ( int i = 0; i <52 ; i++ ) 
+            for (int i = 0; i < 52; i++)
             {
                 int k = R.Next() % 52;
-                Console.WriteLine($"{(Mast)(k/13)} {(Kaart)(k%13)}");
+                Console.WriteLine($"{(Mast)(k / 13)} {(Kaart)(k % 13)}");
             }
 
             // mõtle välja kuidas teha segatud kaardipakk
 
 
             Mast x = (Mast)Enum.Parse(typeof(Mast), "Risti");
-            Console.WriteLine( x );
+            Console.WriteLine(x);
 
             Console.WriteLine((Tunnus)13);
 
             Tunnus t = Tunnus.Punane | Tunnus.Puust | Tunnus.Koliseb;
             Console.WriteLine((int)t);
 
-            t |= Tunnus.Suur; Console.WriteLine(t );
+            t |= Tunnus.Suur; Console.WriteLine(t);
 
             t ^= Tunnus.Koliseb; Console.WriteLine(t);
 
             if ((t & Tunnus.Puust) == Tunnus.Puust) Console.WriteLine("oli puust");
+
+
+            switch (MessageBox.Show(
+                "Tere", "tervituskast",
+                MessageBoxButtons.YesNoCancel
+                ))
+            {
+                case DialogResult.OK:
+                    Console.WriteLine("vajutati OK");
+                    break;
+                case DialogResult.Cancel:
+                    Console.WriteLine("kankeldame");
+                    break; 
+                case DialogResult.No: break; 
+                case DialogResult.Yes:
+                    break;
+                    
+            }
+
+
 
         }
     }
