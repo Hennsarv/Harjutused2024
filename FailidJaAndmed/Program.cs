9592a03d26ae8873583d1177c01f978e8e4412c8
Henn Sarv <sarviktaat@hotmail.com>
2024-02-09 09:41:09 +0200

    täna tegeleme failidega

---
+++ FailidJaAndmed/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FailidJaAndmed
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Räägime mis on failid ja kuidas nendega ringi käiakse
+             * Vaatame, kuidas lugeda ja kirjutada faile, nende sisu
+             * Proovime CSV ja JSON (kes tahab ka XML) failidega mängida
+             * 
+             * ja seejärel proovime
+             * 
+             * kas ja kuidas ja miks toimetada andmebaasidega
+             * 
+             * Datareaderiga
+             * Linq to DB
+             * Entity FW abiga
+             * 
+             * ja siis mõtleme, mis me võiks nädalavahetusel ise valmis ehitada
+             * 
+             */
+
+
+
+        }
+    }
+}
