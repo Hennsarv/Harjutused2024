70b687531f548a554458606da9554151d77d314f
Henn Sarv <sarviktaat@hotmail.com>
2024-02-09 14:09:11 +0200

    Tegime projekti Console ja EF

---
+++ NW_Console2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NW_Console2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            NorthwindEntities2 db = new NorthwindEntities2();
+            db.Categories.ToList().ForEach(c => Console.WriteLine(c.CategoryName));
+
+            db.Categories.Find(8).CategoryName = "Seafood";
+            db.SaveChanges();
+
+            
+
+
+
+
+
+        }
+    }
+}
