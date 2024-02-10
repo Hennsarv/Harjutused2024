223a82eb011fbdbf80db1dfc1f2fd29b3c6580eb
Hennsarv <henn@sarv.ee>
2024-02-10 23:33:43 +0200

    eemaldasin .reg failid repost (lisasin rea .gitignore)

--- EsimeneVeeb/Models/MySpecialModelExtension.cs
+++ EsimeneVeeb/Models/MySpecialModelExtension.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNet.Identity;
+using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
@@ -29,10 +30,17 @@ public partial class NorthwindEntities : DbContext
         {
             string entityConnectionStringFromConfig = ConfigurationManager.ConnectionStrings["NorthwindEntities"].ConnectionString;
 
+            string regKeyPath = @"HKEY_LOCAL_MACHINE\Software\HennTest";
+            string serverName = (string)Registry.GetValue(regKeyPath, "ServerName", null);
+            string user = (string)Registry.GetValue(regKeyPath, "DBUser", null);
+            string password = (string)Registry.GetValue(regKeyPath, "Password", null);
+
+
+
             var entityBuilder = new EntityConnectionStringBuilder(entityConnectionStringFromConfig);
             var builder = new SqlConnectionStringBuilder(entityBuilder.ProviderConnectionString);
-            builder.UserID = "Student";
-            builder.Password = "Pa$$w0rd";
+            builder.UserID = user; 
+            builder.Password = password; 
 
             entityBuilder.ProviderConnectionString = builder.ConnectionString;
             return entityBuilder.ConnectionString;
