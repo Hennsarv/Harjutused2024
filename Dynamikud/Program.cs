bd08ce18b26d514d771fbf75e64a2de27f6132f1
Henn Sarv <sarviktaat@hotmail.com>
2024-02-13 16:03:51 +0200

    mitte et ma oskaks, aga siin sai psut XAMLiga mängitud

---
+++ Dynamikud/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dynamikud
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 77;
+            string s = "miski jutt";
+
+            string test = 
+@"esimene rida
+teine rida
+kolmas rida
+";
+            foreach(string rida in test.Replace("\r","").Split('\n')) 
+            { Console.WriteLine($"=={rida}*"); }
+
+            Console.WriteLine("\nfailis\n");
+
+            foreach(string rida in System.IO.File.ReadAllText(@"C:\Users\sarvi\source\repos\Harjutused2024\Dynamikud\TextFile1.txt").Split('\n'))
+            {
+                Console.WriteLine($"=={rida}*");
+            }
+
+            StringReader sr = new StringReader(test);
+            while(true)
+            {
+                string r = sr.ReadLine();
+                if (r == null) break;
+                Console.WriteLine($"=={r}*");
+            }
+
+        }
+    }
+}
