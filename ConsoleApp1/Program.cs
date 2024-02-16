d7bc657d48df8263490452ffb628ba461d3589bb
Henn Sarv <sarviktaat@hotmail.com>
2024-02-16 13:56:12 +0200

    viimane jupp

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,71 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        class Kujund { }
+        class Ring : Kujund { public double Raadius; }
+        class Ruut : Kujund { public double Küljepikkus; }
+        class Kolmnurk : Kujund { public double Kõrgus; public double Alus; }
+        class Ristkülik : Kujund { public double Kõrgus; public double Laius; }
+
+
+        static (int sum, int count) Arvuta(IEnumerable<int> arvud)
+        {
+            int s = 0; int c = 0;
+            foreach (int _ in arvud) { c++; s += _; }
+            return (s, c);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            // range objekt ja selle kasutamine
+            int[] arvud = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            var osa = arvud[3..];
+            Console.WriteLine(string.Join(", ", osa));
+
+            // switch avaldis
+            var np = DateTime.Now.DayOfWeek switch
+            {
+                DayOfWeek.Sunday => "pühapäev",
+                DayOfWeek.Saturday => "saunapäev",
+                DayOfWeek.Wednesday => "trennipäev",
+                _ => "misiganes"
+            };
+
+            var pilt = new List<Kujund>
+            { new Ring  { Raadius = 1},
+            new Ruut { Küljepikkus = 1},
+            new Kolmnurk { Kõrgus = 2, Alus = 1},
+            new Ristkülik { Kõrgus = 2, Laius = 1},
+            };
+
+            double pindala = 0;
+            foreach(Kujund ku in pilt)
+            {
+                pindala += ku switch
+                {
+                    Ring ring => ring.Raadius * ring.Raadius * Math.PI,
+                    Ruut r => r.Küljepikkus * r.Küljepikkus,
+                    Kolmnurk k => k.Alus * k.Kõrgus / 2,
+                    Ristkülik r => r.Kõrgus * r.Laius ,
+                     _ => 0
+                }; 
+            }
+
+
+            (int ss, _) = Arvuta(arvud);
+            // _ mulükskõik nimeline muutuja
+
+
+
+
+        
+        
+        }
+
+        
+
+    }
+}
---
+++ TestXmlToDocx/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.IO.Compression;
+
+namespace TestXmlToDocx
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            // Sinu dokumendi faili nimi
+            string folder = @"c:\testDoc";
+            string docxFile = "TestXml.docx";
+            string docxInFolder = Path.Combine(folder, docxFile);
+
+            // Sinu XML ja XSD failide nimed
+            string xmlFile = "Kontakt.xml";
+            string xsdFile = "Kontakt.xsd";
+            string xmlInFolder = Path.Combine(folder, xmlFile);
+            string xsdInFolder = Path.Combine(folder, xsdFile);
+
+
+
+            // Ava ZIP-fail
+            using (ZipArchive docxZip = ZipFile.Open(docxInFolder, ZipArchiveMode.Update))
+            {
+                // Lisa XML ja XSD failid customXml kausta
+                docxZip.CreateEntryFromFile(xmlInFolder, $"customXml/{xmlFile}");
+                docxZip.CreateEntryFromFile(xsdInFolder, $"customXml/{xsdFile}");
+            }
+        }
+    }
+}
